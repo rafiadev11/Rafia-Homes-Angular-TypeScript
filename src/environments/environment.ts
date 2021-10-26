5d6d9ad453c9419f5136bdb68bac2e3770318105
Rachid Rafia <62402362+rafiadev11@users.noreply.github.com>
2021-10-26 16:01:05 -0400

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   rapidApiBaseURL: 'https://realtor.p.rapidapi.com/properties/v2',
-  rapidApiKey: '48dbbb20e4msh82584db99d33830p1d0550jsn4dabea002598',
+  rapidApiKey: '',
   rapidApiHost: 'realtor.p.rapidapi.com',
   gmapApiKey: 'AIzaSyBawgJWTcB4BvnbloKqjrHw0GGrOgrKE6w',
 };
